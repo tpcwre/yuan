ab81ad3275c6319c1629bfc1d32c0949806c184f
tpcwre_tkpad <tpcwre_tkpad@163.com>
2016-10-15 15:57:48 +0800

    ..

---
+++ -学习归纳/18-数学函数/--娄学函数整理.php
@@ -0,0 +1,141 @@
+<?php
+	echo '<pre>';
+
+
+	
+	/*
+//-- pi() 获取圆周率
+
+	echo pi();
+	
+	
+	
+//-- sqrt() 求一个数的平方根
+
+	echo sqrt(9);
+	
+	
+//-- number_format() 千位分组格式化数字
+	
+	echo number_format(1234324.534);		//1,234,325		默认四舍五入去除小数
+
+	echo number_format(2325938.325535,2,',',' ');	
+		//2 325 938,33	四舍五入保留2位小数，小数部分使用 , 分隔 ，千分位使用空格 分隔 
+
+	echo number_format(2325938.325535,3,'.',',');	
+		//2,325,938.326 四舍五入保留3位小数，小数部分使用 . 分隔 ，千分位使用空格 , 分隔
+
+
+
+//-- base_convert()	自定义进制转换
+	
+	echo base_convert(11,16,2);		//十六转二	10001
+	echo base_convert(11,10,2);		//十转二	1011
+	echo base_convert(11,8,2);		//八转二	1001
+	echo base_convert(11,10,16);	//十转十六	b
+	echo base_convert(11,8,16);		//八转十六	9
+	echo base_convert(11,2,16);		//二转十六	3
+	echo base_convert(11,16,8);		//十六转八	21
+	echo base_convert(11,10,8);		//十转八	13
+	echo base_convert(11,2,8);		//二转八	3
+	echo base_convert(11,16,10);	//十六转十	17
+	echo base_convert(11,2,10);		//二转十	3
+	echo base_convert(11,8,10);		//八转十	9
+	
+	
+//-- 十六进制转八进制
+
+	echo dechex(13);		//d
+
+
+
+//-- hexdex() 十六进制转十进制
+	
+	echo hexdec('F');		//15
+	
+
+
+
+
+
+	
+//-- mt_getrandmax() 显示mt_rand()随机数的最大可能值
+
+	echo mt_getrandmax();
+
+
+	
+	
+//-- 2147483647 内随机产生一个整数（或指定范围）
+	echo mt_rand();		//随机产生一个10亿内的整数
+	echo mt_rand(1,5);	//1-5随机产生一个整数
+	
+
+
+	
+//-- getrandmax() 显示rand() 随机数最大的可能值
+
+	echo getrandmax();
+
+
+
+	
+	
+//-- rand()  32767内随机产生一个整数（或指定范围内的整数）
+	echo rand();		//32767中随机产生一个数
+	echo rand(1,5);		//1-5随机产生一个数
+
+	
+	
+	
+	
+//-- 计算数值的n次方
+
+	echo pow(2,5);		//32 	求2的5次方
+
+
+
+	
+	
+
+//-- max() 求最大值 
+	echo max(2,4,5,8,10);	//10
+
+
+	
+	
+	
+//- min()  求最小值 
+
+	echo min(1,3,5,7,9);		//1
+	
+	
+	
+	
+
+
+//-- round() 四舍五入取整
+	echo round(12345,-1);	//12350	
+	echo round(15.1254,2);	//15.13
+	echo round(15,49);		//15
+	echo round(12.51);		//13
+
+
+
+//-- ceil() 进一取整 
+
+	echo ceil(12.1);		//13
+
+	
+//-- floor() 舍去取整
+
+	echo floor(23.99);		//23
+	
+	
+	
+//-- abs() 求绝对值 
+
+	echo abs(-234.34);		//234.34
+	
+	
+	*/
