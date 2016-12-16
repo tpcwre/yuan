94b08d17e470a5fbf15f502770259ce94e26996b
tpcwre_tkpad <tpcwre_tkpad@163.com>
2016-12-16 08:12:42 +0800

    ..

--- -学习归纳/17-字符串/--字符串函数整理.php
+++ -学习归纳/17-字符串/--字符串函数整理.php
@@ -1,17 +1,44 @@
 <?php
-echo '<pre>';
-
-
+echo "<pre>";
 
 
 
 	/*
+//-- strspn(str,str,[start,length]) 查找字串2中字符在字串1中连续出现的个数
+	//注意：这里只会统计连续出现字串2中字符的个数，如出现了非串2中的字符，统计将会结束。
+	$a="abcaa";
+	$b="abc";
+	echo strspn($a,$b);		//5 连续找到5个
+
+	$a="abcada";
+	$b="abc";
+	echo strspn($a,$b);		//连续找到4个
+
+	$a="abcdaa";
+	$b="abc";
+	echo strspn($a,$b);		//连续找到3个
+
+	
+	
+	
+	
+//-- strcspn(str,str) 查找串1中连续且非串2中字符的个数
+	$a="abcdefg";
+	$b="dg";
+	echo strcspn($a,$b);		//字串1中连续3个字符未匹配字串2
+
+	$a="abcdefg";
+	$b="efg";
+	echo strcspn($a,$b);		//字串1中连续4个字符未匹配字串2
+
+
+
 
 //-- strtr() 用指定字符替换字串中的指定内容
-	$a = 'abc def';
-	//echo strtr($a,array('b'=>'g'));		//agc def	将b换成了g
-	//echo strtr($a,array(' '=>''));		//abcdef	将空格转成空（去除了中间的空格）
-	echo strtr($a,array('c d'=>'kk'));		//abkkef
+	$a = "abc def";
+	//echo strtr($a,array("b"=>"g"));		//agc def	将b换成了g
+	//echo strtr($a,array(" "=>""));		//abcdef	将空格转成空（去除了中间的空格）
+	echo strtr($a,array("c d"=>"kk"));		//abkkef
 
 
 
@@ -25,7 +52,7 @@ echo '<pre>';
 		%3\$s<br />
 THESTRING;
 
-		$returnText = vprintf($string,array('Oregon','7','I Love Oregon'));
+		$returnText = vprintf($string,array("Oregon","7","I Love Oregon"));
 				//注解： 
 					// %1 = Oregon
 					// %2 = 7
@@ -44,8 +71,8 @@ THESTRING;
 
 
 //-- similar_text()  计算两个字串的相似度
-	$a = 'abcdefghijk';
-	$b = 'sdfjoein;ae';
+	$a = "abcdefghijk";
+	$b = "sdfjoein;ae";
 	similar_text($a,$b,$res);
 	echo $res;		//18.181818181818
 	similar_text($b,$a,$res);
@@ -62,7 +89,7 @@ THESTRING;
 //-- wordwrap() 以指定长度将字串折行处理
 
 	$text = "The quick brown fox jumped over the lazy dog.";
-	echo wordwrap($text,10,'<br><br>');
+	echo wordwrap($text,10,"<br><br>");
 			The quick
 
 			brown fox
@@ -78,13 +105,13 @@ THESTRING;
 
 
 //-- sha1() 将字串加密
-	$a='a';
+	$a="a";
 	echo sha1($a);
 
 
 
 //-- md5() 将字串加密
-	$a='a';
+	$a="a";
 	echo md5($a);
 
 
@@ -93,9 +120,9 @@ THESTRING;
 
 
 //-- http_build_query() 将数组转换成URL键值字串
-	$a = array('aa','bb','cc'=>'ccc');
+	$a = array("aa","bb","cc"=>"ccc");
 	//echo http_build_query($a);		//0=aa&1=bb&cc=ccc
-	echo http_build_query($a,'index_');	//index_0=aa&index_1=bb&cc=ccc
+	echo http_build_query($a,"index_");	//index_0=aa&index_1=bb&cc=ccc
 
 
 
@@ -103,7 +130,7 @@ THESTRING;
 
 //-- ip2long(),long2ip() 将ip字串转为int，或将int转为ip 
 
-	$a = '192.168.1.111';
+	$a = "192.168.1.111";
 	echo $b = ip2long($a);		// -1062731409
 	echo $c = long2ip($b);		// 192.168.1.111
 	
@@ -112,7 +139,7 @@ THESTRING;
 
 //-- urlencode(),urldecode() 对字串进行url编码和解码
 
-	$a = '我是一个字符串';
+	$a = "我是一个字符串";
 	echo $b = urlencode($a);
 	echo $c = urldecode($b);
 	
@@ -138,7 +165,7 @@ THESTRING;
 
 
 //-- pathinfo() 返回文件路径信息
-	$u='http://www.baidu.com/obj/index.php';
+	$u="http://www.baidu.com/obj/index.php";
 	echo pathinfo($u,PATHINFO_DIRNAME);			//http://www.baidu.com/obj
 	echo pathinfo($u,PATHINFO_BASENAME);		// index.php
 	echo pathinfo($u,PATHINFO_FILENAME);		// index
@@ -148,7 +175,7 @@ THESTRING;
 
 
 //-- count_chars() 返回字串中所有字母出现的次数
-	$a = 'abclskajefheskjf';
+	$a = "abclskajefheskjf";
 	$arr = count_chars($a);	//返回的是一个从0到255的ASCLL码统计的数组
 	print_r($arr);		//如 a 出现了两次，统计中就是97 对应的值为 2
 		    [97] => 2
@@ -178,24 +205,24 @@ THESTRING;
 
 
 //-- strlen() 获取字串的长度
-	$a='abcdefg';
+	$a="abcdefg";
 	echo strlen($a);		//7
 
 
 
 //-- str_word_count() 统计字串中含有的单词数
-	$a='this is a very builltfull gril';
+	$a="this is a very builltfull gril";
 	echo str_word_count($a);		//6
 
 
 
 //-- strripos() 返回字串中指定字符最后一次出现的位置，不区分大小写
-	$a='abfcdefg';
-	echo strripos($a,'F');		//6
+	$a="abfcdefg";
+	echo strripos($a,"F");		//6
 
 //-- strrpos()  返回字串中指定字符最后一次出现的位置，区分大小写
-	$a='abecdefg';
-	echo strrpos($a,'e');		//5
+	$a="abecdefg";
+	echo strrpos($a,"e");		//5
 	
 
 
@@ -203,18 +230,18 @@ THESTRING;
 
 
 //-- stripos() 返回字串中指定字符首次出现的位置，不区分大小写
-	$a = 'abcdefg';
-	echo stripos($a,'C');	//2
+	$a = "abcdefg";
+	echo stripos($a,"C");	//2
 
 
 //-- strpos() 返回字串中指定字符首次出现的位置,区分大小写
-	$a='abcdefg';
-	echo strpos($a,'e');	//4
+	$a="abcdefg";
+	echo strpos($a,"e");	//4
 
 
 
 //-- stristr() 返回字串中指定字符首次出现位置到结束的内容,不区分大小写
-	$a='abcdefg';
+	$a="abcdefg";
 	echo stristr($a,D);		//defg
 
 	
@@ -222,23 +249,23 @@ THESTRING;
 	
 
 //-- strstr(),strchr() 返回字串中指定字符首次出现位置到结束的内容,区分大小写
-	$a = 'abcdefg';
-	//echo strstr($a,'b');		//bcdefg	从字串首个b 截取到结尾
-	//echo strchr($a,'c');		//cdefg		从字串首个c 截取到结尾 
-	echo strstr($a,'b',true);	//a			从字串开头截取至首个字符b(前)
+	$a = "abcdefg";
+	//echo strstr($a,"b");		//bcdefg	从字串首个b 截取到结尾
+	//echo strchr($a,"c");		//cdefg		从字串首个c 截取到结尾 
+	echo strstr($a,"b",true);	//a			从字串开头截取至首个字符b(前)
 
 
 
 //-- strrchr() 返回字串中指定内容最后出现的位置到结束的内容
-	$a='abcdefg';
-	echo strrchr($a,'e');		//efg
+	$a="abcdefg";
+	echo strrchr($a,"e");		//efg
 
 
 
 
 
 //-- similar_text() 获取两个字串相同字符的数量
-	$a='abcdefg';
+	$a="abcdefg";
 	$b="abk";
 	echo similar_text($a,$b);		//2
 
@@ -246,11 +273,11 @@ THESTRING;
 
 
 //-- substr_replace()   替换字串中被截取的字串
-	$a='abcdefg'; 
-	$b='ppp';
-	//echo substr_replace($a,'ppp',-2);		//abcdeppp		将倒数第二个字符到结尾的内容替换成ppp
-	//echo substr_replace($a,'ppp',2);	//abppp  	将第二个字符到结尾的内容替换成ppp
-	echo substr_replace($a,'ppp',1,5);	//apppg		将下标为1-5的字符内容替换成ppp
+	$a="abcdefg"; 
+	$b="ppp";
+	//echo substr_replace($a,"ppp",-2);		//abcdeppp		将倒数第二个字符到结尾的内容替换成ppp
+	//echo substr_replace($a,"ppp",2);	//abppp  	将第二个字符到结尾的内容替换成ppp
+	echo substr_replace($a,"ppp",1,5);	//apppg		将下标为1-5的字符内容替换成ppp
 
 	
 	
@@ -259,8 +286,8 @@ THESTRING;
 	
 	
 //-- mb_substr() 截取字符串并指定其编码
-	$a='abcdefg';
-	echo mb_substr($a,3,3,'utf-8');		// def	从下标3 开始，截取3个字符，并指定编码为utf-8
+	$a="abcdefg";
+	echo mb_substr($a,3,3,"utf-8");		// def	从下标3 开始，截取3个字符，并指定编码为utf-8
 	
 	
 	
@@ -270,9 +297,9 @@ THESTRING;
 //-- mb_strlen()  以指定编码获取字符串长度
 	$a = "在下天下无乱敌";
 	echo strlen($a);			//21
-	echo mb_strlen($a,'gbk');	//11
-	echo mb_strlen($a,'utf-8');	//7
-	echo mb_strlen($a,'unicode');	//10
+	echo mb_strlen($a,"gbk");	//11
+	echo mb_strlen($a,"utf-8");	//7
+	echo mb_strlen($a,"unicode");	//10
 	
 	
 	
@@ -292,17 +319,17 @@ THESTRING;
 	
 
 //-- substr_count() 统计指定字符在字串中出现了多少次
-	$a='aluukasfasaluusajfluu';
-	echo substr_count($a,'uu');		//3
+	$a="aluukasfasaluusajfluu";
+	echo substr_count($a,"uu");		//3
 
 
 
 
 //-- str_ireplace() 用字串替换字串或字串中的内容,不区分大小写
-	$a='Abcdefg';
-	//echo str_ireplace('a','jjj',$a);		//jjjbcdefg
-	$y1=array('b','c');
-	$y2=array('m','n');
+	$a="Abcdefg";
+	//echo str_ireplace("a","jjj",$a);		//jjjbcdefg
+	$y1=array("b","c");
+	$y2=array("m","n");
 	echo str_ireplace($y1,$y2,$a,$num);		//Amndefg
 	echo $num;			//2 替换的个数
 
@@ -310,15 +337,15 @@ THESTRING;
 
 
 //-- str_replace() 用字串替换字串或字串中的内容,区分大小写
-	$a = 'abcdefg';
-	echo str_replace('de','ggg',$a);		//abcgggfg
+	$a = "abcdefg";
+	echo str_replace("de","ggg",$a);		//abcgggfg
 
 
 
 //-- substr() 	截取字串
 	$a="abcdefg";
-	//echo substr($a,'3');	//defg	从下标3 截取至结尾
-	//echo substr($a,'-3');	//efg	从倒数第三个截取至结尾 
+	//echo substr($a,"3");	//defg	从下标3 截取至结尾
+	//echo substr($a,"-3");	//efg	从倒数第三个截取至结尾 
 	//echo substr($a,2,3);	//cde 	从下标2开始，截取后面的三个字符
 	echo substr($a,-3,2);	//ef	从倒数第3个字符，截取2个字符
 
@@ -326,8 +353,8 @@ THESTRING;
 
 
 //-- implode() 将数组各元素用指定字符连接成一个字串
-	$a = array('aa','bb','cc');
-	var_dump(implode(',',$a));			//aa,bb,cc
+	$a = array("aa","bb","cc");
+	var_dump(implode(",",$a));			//aa,bb,cc
 
 
 
@@ -337,7 +364,7 @@ THESTRING;
 
 //-- explode() 	将字串以指定字符分割成数组
 	$a="abc,efg,hij";
-	$b = explode(',',$a);
+	$b = explode(",",$a);
 	var_dump($b);
 			array(3) {
 			  [0]=>
@@ -352,8 +379,8 @@ THESTRING;
 
 
 //--  strtok() 切除字串中指定字符以后的内容
-	$a='abceefg';
-	var_dump(strtok($a,'e'));		// abc
+	$a="abceefg";
+	var_dump(strtok($a,"e"));		// abc
 
 
 
@@ -361,15 +388,15 @@ THESTRING;
 
 
 //-- chunk_split() 将字串按指定长度分隔成小块（空格或指定符号分隔） 
-	$a='abcdefg';
+	$a="abcdefg";
 	//echo chunk_split($a,3);		// abc def g 	默认用空格分隔
-	echo chunk_split($a,3,'-');		// abc-def-g-	使用指定符号分隔
+	echo chunk_split($a,3,"-");		// abc-def-g-	使用指定符号分隔
 
 
 
 
 //-- strnatcasecmp() 自然顺序比较两字串差异，不区分大小写
-	$a='Gbc';
+	$a="Gbc";
 	$b="abc";
 	echo strnatcasecmp($a,$b);		//1
 	echo strnatcasecmp($b,$a);		//-1
@@ -386,8 +413,8 @@ THESTRING;
 
 
 //-- strnatcmp() 自然顺序比较两字串差异,区分大小写
-	$a='abdsdkjfsldf';
-	$b='abAsdf';
+	$a="abdsdkjfsldf";
+	$b="abAsdf";
 	echo strnatcmp($a,$b);		//1
 	echo strnatcmp($b,$a);		//-1
 
@@ -401,7 +428,7 @@ THESTRING;
 
 
 //-- strncmp() 比较两字串前N个字符的差异,区分大小写
-	$a='abcdefg';
+	$a="abcdefg";
 	$b="abcflje";
 	echo strncmp($a,$b,5);	//-1
 	echo strncmp($b,$a,5);	// 1
@@ -434,8 +461,8 @@ THESTRING;
 
 
 //-- strcmp() 以下标位置逐对比较两个字串，区分大小写
-	$a='AgFc';
-	$b='agGoe';
+	$a="AgFc";
+	$b="agGoe";
 	echo strcmp($a,$b);		// -1
 	echo strcmp($b,$a);		//  1
 	//按下标位置只比较首对出现差异的字母，后面全部忽略 
@@ -445,8 +472,8 @@ THESTRING;
 
 
 //-- strcasecmp() 以下标位置逐对比较两个字串,不区分大小写
-	$a = 'fbedefghijklaaa';
-	$b = 'abcdefghijklaakfdsfdfds';
+	$a = "fbedefghijklaaa";
+	$b = "abcdefghijklaakfdsfdfds";
 	echo strcasecmp($a,$b);		//  5
 	echo strcasecmp($b,$a);		// -5
 	//两组字串以下标位置逐个字符做比较，
@@ -458,7 +485,7 @@ THESTRING;
 		 
 		 
 //-- ord() 将字符转成ASCLL码
-	$a='a';
+	$a="a";
 	echo ord($a);		// 97
 
 	
@@ -474,15 +501,15 @@ THESTRING;
 
 
 //-- stripslashes() 去除字串中所有反斜线
-	$a = '\\\\\\\\\\\\\\\0\\\\\\\\a\\\\b\\\c\\d\e';		
+	$a = "\\\\\\\\\\\\\\\0\\\\\\\\a\\\\b\\\c\\d\e";		
 	echo stripslashes($a);			// \\\\0\\a\b\cde
 		注：去除字串中的反斜线，但每4个连续的反斜线会保留1个反斜线 
 
 		
 	
 	
-//-- addslashes() 给 ',", \ 前加反斜线
-	$a="\a\\b\\\c\\\\d\\\\\e------'b\bb'";
+//-- addslashes() 给 ",", \ 前加反斜线
+	$a="\a\\b\\\c\\\\d\\\\\e------"b\bb"";
 	echo $b = addslashes($a);		//\\a\\b\\\\c\\\\d\\\\\\e
 		//注： 原有反斜线个数为奇数时才会加反斜线，原反斜线个数为偶数时不会加。
 
@@ -490,7 +517,7 @@ THESTRING;
 	
 
 //-- 将字串中的$,^,*,(,),+,[,],\,.,? 前面添加反斜线
-	$a='!@#$%^&*()_+1234567890qwertyuiop[]\{}|asdfghjkl;:zxcvbnm,./<>?';
+	$a="!@#$%^&*()_+1234567890qwertyuiop[]\{}|asdfghjkl;:zxcvbnm,./<>?";
 	echo quotemeta($a);
 		//!@#\$%\^&\*\(\)_\+1234567890qwertyuiop\[\]\\{}|asdfghjkl;:zxcvbnm,\./<>\?
 
@@ -498,7 +525,7 @@ THESTRING;
 		
 		
 //-- 去除字串中的反斜线，及\r,\n,\v等,虽不显示，但效果还在
-	$a='\\\\k\\\\\\\\k\\\\\\\\\\\\kva\rbc\kd\wkf\naa\kkke  vv\vab\r\n';
+	$a="\\\\k\\\\\\\\k\\\\\\\\\\\\kva\rbc\kd\wkf\naa\kkke  vv\vab\r\n";
 	echo stripcslashes($a);
 		\k\\k\\\va
 		bckdwkf
@@ -510,7 +537,7 @@ THESTRING;
 
 //-- 给字串中指定字符前添加反斜线 
 	$a = "anbcdefnghijklmn";
-	echo addcslashes($a,'nbh');		//a\n\bcdef\ng\hijklm\n
+	echo addcslashes($a,"nbh");		//a\n\bcdef\ng\hijklm\n
 
 	
 	
@@ -531,8 +558,8 @@ THESTRING;
 
 
 //-- htmlspecialchars() 预定义字符转html实体,标签不变但失去功能(同htmlentities)
-	$a="<script>alert('a');</script>";
-	echo htmlspecialchars($a);		//<script>alert('a');</script>
+	$a="<script>alert("a");</script>";
+	echo htmlspecialchars($a);		//<script>alert("a");</script>
 	
 	将标签转为实体，标签正常显示，但失去标签的意义了
 	测试效果和htmlentities一样，没搞明白它俩有什么区别
@@ -558,14 +585,14 @@ THESTRING;
 
 	
 //-- 将标签转为实体,标签不变但失去功能（同htmlspecialchars）
-	$a="<script>alert('a');</script>";
-	echo htmlentities($a);		//<script>alert('a');</script>
+	$a="<script>alert("a");</script>";
+	echo htmlentities($a);		//<script>alert("a");</script>
 
 
 
 
 //-- 字串中每个单词首字母大写
-	$a='this is a new days ';
+	$a="this is a new days ";
 	echo ucwords($a);		//This Is A New Days 
 
 	
@@ -573,14 +600,14 @@ THESTRING;
 	
 
 //-- 字串首字母大写
-	$a='abc';
+	$a="abc";
 	echo ucfirst($a);		//Abc
 
 
 
 
 //-- strtoupper() 将字串转为大写
-	$a = 'abcdefg';
+	$a = "abcdefg";
 	echo strtoupper($a);		//ABCDEFG
 
 
@@ -605,7 +632,7 @@ THESTRING;
 
 
 //-- str_shuffle() 随机打乱字串
-	$a='abcdefg';
+	$a="abcdefg";
 	echo str_shuffle($a);		//bcaedgf
 	
 	
@@ -622,7 +649,7 @@ THESTRING;
 	
 //-- str_split() 将字串按指定长度分割成数组
 	$a = "abcdefhijklm";
-	$b = str_split($a,'7');		//将字串以每3个字符为一组分割成数组 
+	$b = str_split($a,"7");		//将字串以每3个字符为一组分割成数组 
 	var_dump($b);
 			array(2) {
 			  [0]=>
@@ -644,7 +671,7 @@ THESTRING;
 	
 	
 //-- str_repeat() 将字串重复指定次数
-	$a = 'abc';
+	$a = "abc";
 	echo str_repeat($a,3);		//abcabcabc
 	
 	
@@ -653,21 +680,21 @@ THESTRING;
 	
 	
 //-- str_pad() 用指定字符填充字串
-	$a='aaa';
-	//echo str_pad($a,10,'c');		//aaaccccccc		
-	//echo str_pad($a,8,'#',STR_PAD_LEFT);		//#####aaa
-	//echo str_pad($a,8,'#',STR_PAD_RIGHT);		//aaa#####
-	echo str_pad($a,8,'#^',STR_PAD_BOTH);		//#^aaa#^#
+	$a="aaa";
+	//echo str_pad($a,10,"c");		//aaaccccccc		
+	//echo str_pad($a,8,"#",STR_PAD_LEFT);		//#####aaa
+	//echo str_pad($a,8,"#",STR_PAD_RIGHT);		//aaa#####
+	echo str_pad($a,8,"#^",STR_PAD_BOTH);		//#^aaa#^#
 	
 	
 
 	
 //-- trim() 去除字串两侧的空格或指定字符
-	$a= '  ccaacc   ';
-	echo 'b'.trim($a).'b';		//bccaaccb
+	$a= "  ccaacc   ";
+	echo "b".trim($a)."b";		//bccaaccb
 	
-	$c='ccaacc';
-	echo trim($c,'cc');			//aa
+	$c="ccaacc";
+	echo trim($c,"cc");			//aa
 	
 	
 
@@ -675,21 +702,21 @@ THESTRING;
 
 //-- ltrim() 去除字串中左侧的空格或指定字符
 
-	$a='  a  ';
+	$a="  a  ";
 	echo "b".ltrim($a)."b";		//ba  b
 	
 	$c="aaccaa";
-	echo ltrim($c,'aa');		//ccaa
+	echo ltrim($c,"aa");		//ccaa
 	
 
 
 //-- rtrim() 去除字串中右侧的空格或指定字符 ,别名： chop
 
-	$a='  a  ';
+	$a="  a  ";
 	echo "b".rtrim($a)."b";		//b  ab
 
-	$c = 'aaccaa';
-	echo rtrim($c,'a');			//aacc
+	$c = "aaccaa";
+	echo rtrim($c,"a");			//aacc
 	
 
 
